d26a10728e0489be8e2893a2ad03bf54e902aaa7
twwli <hello@theworldwelivein.co.uk>
2021-12-03 10:29:33 +0100

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+
+var accordion = $('.accordion');
+
+accordion.mouseenter(function(){
+  $(this).addClass('active-acc');
+  accordion.not(this).addClass('not-acc');
+}).mouseleave(function(){
+   $(this).removeClass('active-acc');
+   accordion.not(this).removeClass('not-acc');
+});
+
+var sectionTrigger = $('.section-trigger');
+
+sectionTrigger.on('click', function(event){
+	event.preventDefault();
+	$(this).next('.section-content').slideToggle(400).end().parent('.block').toggleClass('content-visible');
+  sectionTrigger.not(this).next('.section-content').slideUp(400);
+  
+	$(this).toggleClass('is-active');
+  $('.block').not(this).removeClass('content-visible');
+  sectionTrigger.not(this).removeClass('is-active');
+});
+
+
+}); /* End Document Ready */
