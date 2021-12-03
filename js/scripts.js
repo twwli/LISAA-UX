d25f3b1d43357529ffaf1e6403479e22b711a1ea
twwli <hello@theworldwelivein.co.uk>
2021-12-03 16:37:51 +0100

    Updated list

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,11 @@ var accordion = $('.accordion');
 accordion.mouseenter(function(){
   $(this).addClass('active-acc');
   accordion.not(this).addClass('not-acc');
+  $('header, footer').addClass('not-acc');
 }).mouseleave(function(){
    $(this).removeClass('active-acc');
    accordion.not(this).removeClass('not-acc');
+   $('header, footer').removeClass('not-acc');
 });
 
 var sectionTrigger = $('.section-trigger');
