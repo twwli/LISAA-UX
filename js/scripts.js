a5b475e1d2da199121d7e500081f10a4d3d00aee
twwli <hello@theworldwelivein.co.uk>
2025-01-17 13:41:57 +0100

    Adjust balloon rotation

--- js/scripts.js
+++ js/scripts.js
@@ -159,11 +159,9 @@ var accordion = $('.accordion');
 accordion.mouseenter(function(){
   $(this).addClass('active-acc');
   accordion.not(this).addClass('not-acc');
-  $('header, footer').addClass('not-acc');
 }).mouseleave(function(){
    $(this).removeClass('active-acc');
    accordion.not(this).removeClass('not-acc');
-   $('header, footer').removeClass('not-acc');
 });
 
 var sectionTrigger = $('.section-trigger');
@@ -177,6 +175,5 @@ sectionTrigger.on('click', function(event){
   $('.block').not(this).removeClass('content-visible');
   sectionTrigger.not(this).removeClass('is-active');
 });
-
 
 }); /* End Document Ready */
